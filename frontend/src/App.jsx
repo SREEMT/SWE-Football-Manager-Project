83f089260ef347e541de0ab70f087731de367e6b
aweso <jliim39882@gmail.com>
2025-10-27 00:01:02 -0400

    Updated frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,27 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+//import viteLogo from '/vite.svg'
+import WebLogo from './assets/WebLogo.png'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [isOpen, setIsOpen] = useState(false);
 
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
+
+  
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+      <a target="_blank">
+          <img src={WebLogo} className="logo" alt="Logo" />
         </a>
+      <div>
+        {/* <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a> */}
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
@@ -21,15 +31,36 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
+        {/* <p>
           Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        </p> */}
       </div>
-      <p className="read-the-docs">
+      {/* <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
+      </p> */}
+
+      {/* <div style="overflow-y: scroll; height: 200px; width: 300px; border: 1px solid black;">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vehicula, lorem non tincidunt fermentum, sapien justo tincidunt libero, nec tincidunt ligula magna vel eros. Suspendisse potenti
+      </div> */}
+
+      <div style={{ position: "relative", display: "inline-block" }}>
+      {/* Button to toggle dropdown */}
+      <button onClick={toggleDropdown} className="menudropdowndutton">
+        Menu
+      </button>
+
+      {/* Dropdown items */}
+      {isOpen && (
+        <ul className="menudropdown">
+          <li className="item"><a href="#" target="_blank">Link</a></li>
+          <li className="item"><a href="#" target="_blank">Link</a></li>
+          <li className="item"><a href="#" target="_blank">Link</a></li>
+        </ul>
+        )}
+      </div>
     </>
   )
 }
+
 
 export default App
