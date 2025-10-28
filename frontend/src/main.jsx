010104f2274c0d82542f619178e9f34cf4030a53
SREEMT <tmeers101@gmail.com>
2025-10-28 12:43:42 -0400

    Merged Login-Logout branch

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
-=======
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
@@ -24,4 +12,3 @@ createRoot(document.getElementById('root')).render(
     </BrowserRouter>
   </StrictMode>,
 )
->>>>>>> login-logout-iter-1
