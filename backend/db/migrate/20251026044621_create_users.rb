bd09ea01ca67e8e8e2b90b1da4051a06aeab5a7d
Seje7 <oseje2003@gmail.com>
2025-11-06 07:00:08 -0500

    Created more columns for sign up

--- backend/db/migrate/20251026044621_create_users.rb
+++ backend/db/migrate/20251026044621_create_users.rb
@@ -3,6 +3,11 @@ class CreateUsers < ActiveRecord::Migration[8.0]
     create_table :users do |t|
       t.string :email_address, null: false
       t.string :password_digest, null: false
+      t.string :first_name, null: false
+      t.string :last_name, null: false
+      t.string :phone_number, null: false
+      t.integer :role, default: 1, null: false
+      t.date :date_of_birth, null: false
 
       t.timestamps
     end
