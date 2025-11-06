13477ec66c4a9b574ae113b4d04d256c6c3586e7
Seje7 <oseje2003@gmail.com>
2025-11-05 19:54:54 -0500

    Changed css code

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import NotFound from './NotFound'
-import './Login.css'
+// import './Login.css'
 import { Link } from 'react-router-dom'  
 
 export default function Login() {
