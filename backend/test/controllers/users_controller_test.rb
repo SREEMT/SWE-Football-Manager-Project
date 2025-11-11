9ddf26568d3bd9d6f8409adecfee03e7b0ea2111
Seje7 <oseje2003@gmail.com>
2025-11-11 12:43:47 -0500

    added test for new session when user is created

--- backend/test/controllers/users_controller_test.rb
+++ backend/test/controllers/users_controller_test.rb
@@ -27,12 +27,13 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     }
   end
 
- test "should get index and return all users" do
+=begin test "should get index and return all users" do
   get users_url, as: :json
   assert_response :ok
   json = JSON.parse(response.body)
   assert_kind_of Array, json
 end
+=end 
 
   test "POST /users with valid parameters creates a user" do
     assert_difference("User.count", 1) do
@@ -41,7 +42,7 @@ end
 
     assert_response :created
     json = JSON.parse(response.body)
-    assert_equal "User created successfully", json["message"]
+    assert_equal "User created successfully and logged in", json["message"]
     assert_equal "david2009@gmail.com", json["user"]["email_address"]
     assert_nil json["user"]["password_digest"], "Response should not include password_digest"
   end
@@ -56,10 +57,22 @@ end
     assert_includes json["message"], "could not do the sign up"
   end
 
-  test "DELETE /users/logout resets session" do
+=begin  test "DELETE /users/logout resets session" do
     delete "/logout", as: :json
     assert_response :ok
     json = JSON.parse(response.body)
     assert_equal "Logged out", json["message"]
   end
+=end
+
+test "POST /users with valid_parameters_are_created_and_logged_in" do
+  assert_difference("User.count", 1) do
+    post users_url, params: @valid_user_params, as: :json
+  end
+
+  assert_response :created
+  json = JSON.parse(response.body)
+  assert_equal "User created successfully and logged in", json["message"]
+  assert_equal "david2009@gmail.com", json["user"]["email_address"]
+end
 end
