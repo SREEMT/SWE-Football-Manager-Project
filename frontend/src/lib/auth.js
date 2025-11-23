89e4e33d8d22497f2ec868bc44615ea636d5bc6e
Seje7 <oseje2003@gmail.com>
2025-11-23 16:29:56 -0500

    corected js file  spelling error

--- frontend/src/lib/auth.js
+++ frontend/src/lib/auth.js
@@ -31,7 +31,7 @@ export async function logout() {
 
 // Retrieve user session & data
 export async function checkSession() {
-    const response = await fetch(`${API_URL}/sessin`, {
+    const response = await fetch(`${API_URL}/session`, {
         method: `GET`,
         credentials: `include`,
     })
