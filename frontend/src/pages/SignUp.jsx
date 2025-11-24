8d9bc48103a9faea3d5ba2247ed82a40aa897fd5
Seje7 <oseje2003@gmail.com>
2025-11-24 10:29:28 -0500

    created textfields for sign-up functionality

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,86 +1,75 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
+import '../styles/SignUp.css'
 
 export default function SignUp() {
-    const [form, setForm] = useState({
-    email: "",
-    age: "",
-    parentEmail: ""});
-    const [error, setError] = useState(null)
-    const [message, setMessage] = useState("");
-    const navigate = useNavigate();
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState("");
+    const [confirmPassword, setconfirmPassword] = useState("")
+    const [firstName, setFirstName] = useState("")
+    const [lastName, setlastName] = useState("")
+    const [phoneNumber, setphoneNumber] = useState("")
+    const [dob, setDob] = useState("")
 
-    const handleChange = (e) => {
-        setForm({ ...form, [e.target.name]: e.target.value });
-    };
 
-    // Function that calls API
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        setError(null)
-        console.log('Sign up form submitted:', {form})
-
-        {/*
-        try {
-            const success = await signin(email, password)
-            if (success) {
-                console.log('Signin successful')
-                navigate('/', { replace: true})
-            } else {
-                setError('Invalid Credentials')
-            }
-        } catch (err) {
-            console.error('Login error:', err)
-            setError(err.message)
-        }
-            */}
-    };
-
-    const isMinor = form.age && Number(form.age) < 18;
 
     return (
-        <div className='signInClass'>
-            <h2>Create an Account</h2>
-            <form onSubmit={handleSubmit}>
-                <label>Email:</label>
+        <div className="SingUpPage">
+
+            <header className="signUpHeader">
+                <h1>SIGN UP</h1>
+            </header>
+
+            <div className="signupFields">
                 <input
-                    name="email"
                     type="email"
-                    value={form.email}
-                    onChange={handleChange}
-                    required
-                />
+                    placeholder="Email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)} />
 
-                <label>Age:</label>
                 <input
-                    name="age"
-                    type="number"
-                    value={form.age}
-                    onChange={handleChange}
-                    required
+                    type="Password"
+                    placeholder="Enter AlphaNumerical password here"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)} />
+
+                <input
+                    type="Password"
+                    placeholder="Confirm Password"
+                    value={confirmPassword}
+                    onChange={(e) => setconfirmPassword(e.target.value)} />
+
+                <input
+                    type="text"
+                    placeholder="Enter First Name"
+                    value={firstName}
+                    onChange={(e) => setFirstName(e.target.value)} />
+
+                <input
+                    type="text"
+                    placeholder="Enter Last Name"
+                    value={lastName}
+                    onChange={(e) => setlastName(e.target.value)} />
+
+                <input
+                    type="date"
+                    value={dob}
+                    onChange={(e) => setDob(e.target.value)}
                 />
 
-                {isMinor && (
-                    <>
-                    <label>Parent Email:</label>
-                    <input
-                        name="parentEmail"
-                        type="email"
-                        value={form.parentEmail}
-                        onChange={handleChange}
-                        required
-                    />
-                    </>
-                )}
-            
-            <button type="submit">Sign Up</button>
-            </form>
-            <p>
-                Already have an account? <Link to="/login">Sign In</Link>
-            </p>
+                <input
+                    type="tel"
+                    placeholder="Enter Phone Number"
+                    value={phoneNumber}
+                    onChange={(e) => setphoneNumber(e.target.value)}
+                    pattern="[0-9]{10}"
+                />
+
+
+            </div>
+
         </div>
-    );
+    )
 
 }
- 
